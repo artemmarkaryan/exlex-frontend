be2d3d25a514e04c9c52976b97eaa6ae2ebe2881
Markaryan Artem <a.markaryan@vk.team>
2023-03-15 00:14:17 +0300

    bootstrap added

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,3 +1,14 @@
+import Button from 'react-bootstrap/Button';
+
+function MyButton() {
+  return (
+    <Button variant="primary">Click me</Button>
+  );
+}
+
 export const App = () => {
-    return <div>hello vite application</div>;
+    return <>
+        <Button>i am bootstrap button</Button>
+        <div>hello vite application</div>;
+    </>
 };
