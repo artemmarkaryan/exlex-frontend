e31a630669cb88bef5ea84ca3732a06d38343757
artemmarkaryan <artemmarkaryan@yandex.ru>
2023-04-20 23:47:26 +0300

    refactor customer profile

--- src/pages/CustomerProfile/index.tsx
+++ src/pages/CustomerProfile/index.tsx
@@ -7,77 +7,91 @@ import {
     Alert,
     Col,
 } from 'react-bootstrap';
-import React, { useState } from 'react';
-import { gql, useQuery, useMutation } from '@apollo/client';
-import { useAtom } from 'jotai';
-import { tokenDataAtom } from '@/stores/auth';
+import { FormEvent, useEffect, useState } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
 import { GET_CUSTOMER, SET_CUSTOMER } from '@/requests';
+import { Loading } from '@/components/Loading';
 
-export const CustomerProfile: React.FC = () => {
-    const [name, setName] = useState('');
-    const [error, setError] = useState('');
-    const [success, setSuccess] = useState('');
-    const [td, _] = useAtom(tokenDataAtom);
+export const Content = () => {
+    const [name, setName] = useState<string | null>(null);
+    const [succcessMessageShown, setSuccessMessageShown] = useState(false);
+    const [setCustomer, mutResult] = useMutation(SET_CUSTOMER);
 
-    useQuery(GET_CUSTOMER, {
-        onCompleted: (data: any) => {
-            setName(data.selfCustomerProfile.fullName);
-        },
-        onError: (error: any) => {
-            setError('get customer: ' + error.message);
-        },
-    });
-
-    const [setCustomer] = useMutation(SET_CUSTOMER, {
-        variables: { data: { fullName: name } },
-        onCompleted: (data: any) => {
-            setSuccess('Данные профиля обновлены');
-        },
-        onError: (error: any) => {
-            setError('set customer: ' + error.message);
-        },
-    });
-
-    const handleSubmit = (e: any) => {
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        setCustomer();
+        setCustomer({ variables: { data: { fullName: name } } });
+
+        setSuccessMessageShown(true);
+        setInterval(() => {
+            setSuccessMessageShown(false);
+        }, 3000);
     };
 
+    const getCustomerResult = useQuery(GET_CUSTOMER);
+    useEffect(() => {
+        setName(getCustomerResult.data?.selfCustomerProfile?.fullName);
+    }, [getCustomerResult.data]);
+
+    if (getCustomerResult.loading) return <Loading />;
+    if (getCustomerResult.error)
+        return (
+            <Alert variant="danger">
+                Ошибка: {getCustomerResult.error.message}
+            </Alert>
+        );
+
+    return (
+        <Stack gap={2}>
+            <Row>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group>
+                        <Form.Label>Название организации</Form.Label>
+                        <Form.Control
+                            required={true}
+                            type="text"
+                            placeholder="Название организации"
+                            value={name ? name : ''}
+                            onChange={(e) => {
+                                setName(e.target.value);
+                            }}
+                        />
+                    </Form.Group>
+                    <Button
+                        variant="primary"
+                        type="submit"
+                        className="mb-3 mt-3"
+                    >
+                        Сохранить
+                    </Button>
+                </Form>
+            </Row>
+            <Row>
+                <Col>
+                    {mutResult?.error && (
+                        <Alert variant="danger">
+                            Ошибка: {mutResult.error.message}
+                        </Alert>
+                    )}
+                    {mutResult?.data && succcessMessageShown && (
+                        <Alert variant="success">
+                            Данные профиля обновлены
+                        </Alert>
+                    )}
+                </Col>
+            </Row>
+        </Stack>
+    );
+};
+
+export const CustomerProfile = () => {
     return (
         <Container>
-            <Stack gap={2}>
-                <Row>
-                    <h1>Профиль</h1>
-                </Row>
-                <Row>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group>
-                            <Form.Label>Название организации</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Название организации"
-                                value={name}
-                                onChange={(e) => {
-                                    setName(e.target.value);
-                                }}
-                            />
-                        </Form.Group>
-                        <Button
-                            variant="primary"
-                            type="submit"
-                            className="mb-3 mt-3"
-                        >
-                            Сохранить
-                        </Button>
-                    </Form>
-                </Row>
-                <Row>
-                    <Col>
-                        {error && <Alert variant="danger">{error}</Alert>}
-                        {success && <Alert variant="success">{success}</Alert>}
-                    </Col>
-                </Row>
-            </Stack>
+            <Row>
+                <h1>Профиль</h1>
+            </Row>
+            <Row>
+                <Content />
+            </Row>
         </Container>
     );
 };
