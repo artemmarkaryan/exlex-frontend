71588c5e4ffbf11f1efb3ca61fe0a8fa0968fb8a
kir.v.ivanov <kir.v.ivanov@tinkoff.ru>
2023-04-28 02:17:50 +0300

    chore: fix type erro

--- global.d.ts
+++ global.d.ts
@@ -2,6 +2,12 @@ declare module '*.scss' {
     const content: Record<string, string>;
     export default content;
 }
+
+declare module '*.svg' {
+    const content: Record<string, string>;
+    export default content;
+}
+
 interface ImportMetaEnv {
     readonly VITE_SERVER_LOCATION: string;
 }
